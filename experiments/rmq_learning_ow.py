c6b8ebbca5db091a8902f7c29a201e0896485d4f
Luiz Thomasini <luizthomasini@gmail.com>
2022-11-23 20:21:29 -0300

    Added experiments folder

---
+++ experiments/rmq_learning_ow.py
@@ -0,0 +1,64 @@
+from route_choice_gym.route_choice import RouteChoice
+from route_choice_gym.problem import ProblemInstance
+
+from route_choice_gym.agents.rmq_learning import RMQLearning
+from route_choice_gym.strategy import EpsilonGreedy
+
+
+def create_env():
+    P = ProblemInstance('OW')
+    return RouteChoice(P)
+
+
+def main():
+    # initiate environment
+    env = create_env()
+    n_agents_per_od = env.n_of_agents_per_od
+    alpha = 1.0
+    epsilon = 1.0
+
+    print(f"Number of agents: {n_agents_per_od}")
+    print(f"Action space: {env.action_space}")
+    # print(f"Observation space: {env.observation_space}")
+
+    # create agents
+    D = []
+    for od, n in n_agents_per_od.items():
+
+        actions = range(env.action_space[od].n)
+        print(f'Creating {n} drivers for route {od} with the set of actions {actions}')
+
+        for _ in range(n):
+            D.append(RMQLearning(od, actions, EpsilonGreedy(actions, epsilon=epsilon)))
+
+    # assign drivers to environment
+    env.set_drivers(D)
+    print(f'Created {len(env.drivers)} total drivers')
+
+    print("\n")
+    obs_n = env.reset()
+    for _ in range(5):
+
+        # query for action from each agent's policy
+        act_n = []
+        for i, d in enumerate(env.drivers):
+            act_n.append(d.choose_action( obs_n[i] ))
+
+        print("\n")
+        print("-- Debugging ...")
+        print(f"act_n: {act_n}")
+
+        # step environment
+        obs_n_, reward_n, terminal_n = env.step(act_n)
+
+        print(f"obs_n_: {obs_n_}")
+        print(f"reward_n: {reward_n}")
+
+        for i, d in enumerate(env.drivers):
+            d.update_strategy(obs_n_[i], reward_n[i])
+
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
