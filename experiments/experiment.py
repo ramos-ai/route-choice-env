62b917ac2992a6cce2d2d05bce2e52ab83fc752d
Luiz Thomasini <luizthomasini@gmail.com>
2023-01-29 17:02:01 -0300

    Refactored some experiments files

---
+++ experiments/experiment.py
@@ -0,0 +1,85 @@
+import os
+from concurrent.futures import ProcessPoolExecutor
+
+
+class Experiment(object):
+    """
+        Interface for implementing experiments.
+
+        Every method is already defined in this class. It is only needed to implement the run_replication function on
+        its subclasses.
+    """
+    _ID: int
+    ALG: str
+    ITERATIONS: int
+    NET: str
+    K: float
+    REP: int
+    DECAY: float
+    ALPHA: float
+    ALPHA_DECAY: float
+    MIN_ALPHA: float
+    EPSILON: float
+    EPSILON_DECAY: float
+    MIN_EPSILON: float
+
+    def __init__(self, _id: int, algorithm: str, episodes: int, net: str, k: int, decay: float, rep: int):
+        self._ID = _id
+        self.ALG = algorithm
+        self.ITERATIONS = episodes
+        self.NET = net
+        self.K = k
+        self.DECAY = decay
+        self.REP = rep
+        self.ALPHA = 1.0
+        self.ALPHA_DECAY = decay
+        self.MIN_ALPHA = 0.0
+        self.EPSILON = 1.0
+        self.EPSILON_DECAY = decay
+        self.MIN_EPSILON = 0.0
+
+        # LOG
+        self.LOG_V = '00'
+        self.LOGPATH = self.__create_log_path()
+
+    @property
+    def results_summary_filename(self):
+        return f'{self.LOGPATH}/results_v{self.LOG_V}_summary.txt'
+
+    def run(self):
+        for r in range(1, self.REP + 1):
+            self.run_experiment(r)
+
+    def run_multiprocess(self, workers):
+        futures = {}
+        results = {}
+        with ProcessPoolExecutor(max_workers=workers) as ex:
+            for rep in range(1, self.REP + 1):
+                futures[rep] = ex.submit(self.run_experiment, r_id=rep)
+
+        for rep, future in futures.items():
+            results[rep] = future.result()
+
+        with open(self.results_summary_filename, 'a+') as log:
+            log.write(f'Results\t{self.ALG}\t{self.NET}\n')
+            log.write('rep\tavg-tt\treal\test\tabsdiff\treldiff\n')  # \tproximityUE\n')
+            for rep, result in results.items():
+                # result is a vector with 5 indices:
+                # 0:    avg-tt
+                # 1:    real regret
+                # 2:    est regret
+                # 3:    absolute diff between the est and real regrets
+                # 4:    relative diff between the est and real regrets
+                log.write(f'{rep}\t{result[0]}\t{result[1]}\t{result[2]}\t{result[3]}\t{result[4]}\n')
+
+    def run_experiment(self, r_id: int):
+        raise NotImplementedError
+
+    def __create_log_path(self):
+        logpath = f'{os.path.dirname(os.path.abspath(__file__))}'
+        paths = ['/results', f'/{self.ALG}', f'/{self.NET}', f'/K_{self.K}', f'/DECAY_{self.DECAY}']
+        for _path in paths:
+            logpath += _path
+            if not os.path.exists(logpath):
+                os.mkdir(logpath)
+        return logpath
---
+++ experiments/experiment_executor.py
@@ -0,0 +1,66 @@
+"""
+    Main process for running experiments on the route choice environment.
+
+    Make sure to have your experiment implemented and your agent configured on the valid_experiments_alg object.
+"""
+import json
+import timeit
+import argparse
+
+from experiments.experiment import Experiment
+
+from experiments.rmq_learning_exp import RMQLearningExperiment
+from experiments.tq_learning_exp import TQLearningExperiment
+
+
+class InvalidAlgorithm(Exception):
+    pass
+
+
+def run_experiment(Experiment, workers):
+    with open('experiments_config.json', 'r') as file:
+        raw_experiments = json.load(file)
+
+    _id = 1
+    experiments = []
+    for _exp in raw_experiments:
+        for net in _exp['net']:
+            for k in _exp['k']:
+                for decay in _exp['decays']:
+                    experiments.append(Experiment(_id, _exp['episodes'], net, k, decay, _exp['rep']))
+                    _id += 1
+
+    global N_EXPERIMENTS
+    N_EXPERIMENTS = len(experiments)
+
+    print(f'Running {N_EXPERIMENTS} experiments...')
+
+    for exp in experiments:
+        if workers > 1:
+            exp.run_multiprocess(workers)
+        else:
+            exp.run()
+
+
+if __name__ == '__main__':
+    valid_experiments_alg = {
+        'RMQLearning': RMQLearningExperiment,
+        'TQLearning': TQLearningExperiment
+    }
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--alg", required=True)
+    parser.add_argument("--workers", type=int, default=1)
+    args = parser.parse_args()
+
+    try:
+        EXP: Experiment = valid_experiments_alg[args.alg]
+        workers = int(args.workers)
+    except KeyError:
+        raise InvalidAlgorithm(f"Invalid algorithm provided: {args.alg}. "
+                               f"\nChoose one from the list: {valid_experiments_alg.keys()}")
+
+    starttime = timeit.default_timer()
+    print("The start time is :", starttime)
+    run_experiment(EXP, args.workers)
+    print("The time difference is :", timeit.default_timer() - starttime)
