0023d49b3388feab8a6f80da80ef069841601ceb
Luiz Thomasini <luizthomasini@gmail.com>
2023-02-01 11:50:52 -0300

    test

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='route-choice-env',
+setup(
+      name='route-choice-env',
       version='0.0.1',
       description='Multi-Agent RL Route Choice Environment',
       url='https://github.com/ramos-ai/route-choice-gym',
