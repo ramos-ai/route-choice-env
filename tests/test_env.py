a665c1458681cf1ae24d09152366e95ad157e0ee
Luiz Thomasini <luizthomasini@gmail.com>
2023-02-15 22:41:14 -0300

    Fix tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,14 +1,6 @@
-from gymnasium.utils.env_checker import check_env
 from pettingzoo.test import parallel_api_test  # noqa: E402
 
 from route_choice_env.route_choice import RouteChoice, RouteChoicePZ
-
-
-def test_gym_env():
-    # TODO
-    #   env = RouteChoice()
-    #   check_env(env, skip_render_check=True)
-    raise NotImplementedError
 
 
 def test_pz_env():
