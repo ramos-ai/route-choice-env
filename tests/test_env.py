2c16ca4eccd8cf39816ae7e66ee95a16f183d814
luizth <luizthomasini@gmail.com>
2023-05-06 13:35:38 -0300

    Fix test

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,6 +1,6 @@
 from pettingzoo.test import parallel_api_test  # noqa: E402
 
-from route_choice_env.route_choice import RouteChoice, RouteChoicePZ
+from route_choice_env.route_choice import RouteChoicePZ
 
 
 def test_pz_env():
